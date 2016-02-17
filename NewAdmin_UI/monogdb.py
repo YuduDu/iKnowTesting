9b3454a2c28e3c5d96a5c8ce1c815f1ecb759794
YuduDu <dyd12315@163.com>
2016-02-17 17:12:40 -0600

    monogdb move to my ups

--- NewAdmin_UI/monogdb.py
+++ NewAdmin_UI/monogdb.py
@@ -3,7 +3,7 @@ from pymongo import MongoClient
 class mongodb:
     def __init__(self):
         try:
-            self.client = MongoClient('localhost',27017)
+            self.client = MongoClient('ericdu.com',27017)
             self.db = self.client.iknowTest
         except:
             print "Failed to connect MonogDB."
